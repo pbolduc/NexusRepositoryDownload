9637e0a2f301172329c8c5a5bf5c26c5bfc40e47
Phil Bolduc <philbolduc@gmail.com>
2021-06-16 20:28:57 -0700

    Sample app to pull all the packages from a nexus repo

---
+++ NexusExport/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using NuGet.Common;
+
+namespace NexusExport
+{
+    public class Logger : ILogger
+    {
+        public static ILogger None = new Logger();
+
+        public void Log(LogLevel level, string data)
+        {
+        }
+
+        public void Log(ILogMessage message)
+        {
+        }
+
+        public Task LogAsync(LogLevel level, string data)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task LogAsync(ILogMessage message)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void LogDebug(string data)
+        {
+        }
+
+        public void LogError(string data)
+        {
+        }
+
+        public void LogInformation(string data)
+        {
+        }
+
+        public void LogInformationSummary(string data)
+        {
+        }
+
+        public void LogMinimal(string data)
+        {
+        }
+
+        public void LogVerbose(string data)
+        {
+        }
+
+        public void LogWarning(string data)
+        {
+        }
+    }
+}
---
+++ NexusExport/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+
+namespace NexusExport
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string saveToDirectory = "C:\\packages";
+
+            SearchFilter searchFilter = new SearchFilter(includePrerelease: true);
+
+            string sourceValue = "https://something/repository/nuget-hosted/";
+            PackageSource source = new PackageSource(sourceValue);
+            source.Credentials = new PackageSourceCredential(sourceValue, "username", "password", true, null);
+
+            SourceRepository repository = Repository.Factory.GetCoreV2(source);
+
+            IEnumerable<IPackageSearchMetadata> packageSearchMetadatas = await SearchAsync(searchFilter, repository);
+
+            await DownloadAsync(saveToDirectory, repository, packageSearchMetadatas);
+        }
+
+        private static async Task<IEnumerable<IPackageSearchMetadata>> SearchAsync(SearchFilter searchFilter, SourceRepository repository)
+        {
+            PackageSearchResource resource = await repository.GetResourceAsync<PackageSearchResource>();
+            IEnumerable<IPackageSearchMetadata> packageSearchMetadatas = await resource.SearchAsync(string.Empty, searchFilter, 0, 1000, Logger.None, CancellationToken.None);
+            return packageSearchMetadatas;
+        }
+
+        private static async Task DownloadAsync(string saveToDirectory, SourceRepository repository, IEnumerable<IPackageSearchMetadata> packageSearchMetadatas)
+        {
+            var downloadContext = new PackageDownloadContext(new SourceCacheContext());
+            DownloadResource downloadResource = await repository.GetResourceAsync<DownloadResource>();
+
+            foreach (var packageSearchMetadata in packageSearchMetadatas)
+            {
+                var versions = await packageSearchMetadata.GetVersionsAsync();
+                foreach (var version in versions)
+                {
+                    var identity = version.PackageSearchMetadata.Identity;
+                    DownloadResourceResult downloadResult = await downloadResource.GetDownloadResourceResultAsync(identity, downloadContext, saveToDirectory, Logger.None, CancellationToken.None);
+                }
+            }
+        }
+    }
+}
